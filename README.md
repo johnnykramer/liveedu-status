d035ea65a89b8a3b339cb3507967ef4e98ea4308
johnnykramer <johnnyfearmekramer@gmail.com>
2017-07-16 01:08:18 +0800

    🔥 First commit. version 1.0.0

---
+++ README.md
@@ -0,0 +1,13 @@
+## Get status of LiveEdu channel via Promise!
+### You just need to know the nickname.
+
+    status.username(username:String)
+
+#### Example:
+
+      const status = require('liveedu-status');
+    
+      status.username('johnnykramer')
+        .then(() => {
+          console.log(status)
+        })
---
+++ index.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const _ = require('lodash');
+const domParser = require('dom-parser');
+const parser = new domParser();
+
+function username(username) {
+  let userlink = `https://liveedu.tv/${username}/profile`;
+  let nodes;
+  return new Promise((resolve, reject) => {
+    request(userlink, function (error, response, body) {
+      const dom = parser.parseFromString(body);
+      nodes = dom.getElementsByClassName('live');
+      if (_.isEmpty(nodes)) {
+        status = 'offline';
+        resolve(status);
+      }
+      nodes.forEach((el, i) => {
+        if (nodes.length == 1) {
+          let badge = el.innerHTML;
+          if (badge == 'Live') {
+            status = 'live';
+            resolve(status);
+          } else {
+            status = 'error';
+            resolve(status);
+          }
+        }
+      })
+    })
+  })
+}
+
+
+module.exports = {
+  username,
+}
